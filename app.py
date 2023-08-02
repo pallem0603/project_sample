6cd27135982fe56335a2100cfe1d80e1af714b65
pallem0603 <pallem@uab.edu>
2023-08-01 22:56:51 -0500

    redirect commit

--- app.py
+++ app.py
@@ -27,7 +27,14 @@ inventory = {
 
 @app.route('/')
 def main_page():
-    return render_template('main_page.html')
+    # Check if user is already logged in
+    # Redirect to respective dashboard if logged in, otherwise redirect to login page
+    if 'user' in request.cookies:
+        return redirect(url_for('user_dashboard'))
+    elif 'manager' in request.cookies:
+        return redirect(url_for('manager_dashboard'))
+    else:
+        return redirect(url_for('user_login'))
 
 @app.route('/category/<category>')
 def category_menu(category):
@@ -36,71 +43,7 @@ def category_menu(category):
 
     return render_template('category_menu.html', category=category, menu=menu, inventory=inventory)
 
-@app.route('/user/login', methods=['GET', 'POST'])
-def user_login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-
-        user = users.get(username)
-        if user and check_password_hash(user['password'], password):
-            # Successful login, redirect to user dashboard
-            return redirect(url_for('user_dashboard'))
-
-    return render_template('user_login.html')
-
-@app.route('/user/signup', methods=['GET', 'POST'])
-def user_signup():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-
-        if username in users:
-            return "Username already exists. Please choose a different username."
-
-        # Store the user details in the dictionary
-        users[username] = {'username': username, 'password': generate_password_hash(password)}
-        return redirect(url_for('user_dashboard'))
-
-    return render_template('user_signup.html')
-
-@app.route('/manager/login', methods=['GET', 'POST'])
-def manager_login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-
-        manager = managers.get(username)
-        if manager and check_password_hash(manager['password'], password):
-            # Successful login, redirect to manager dashboard
-            return redirect(url_for('manager_dashboard'))
-
-    return render_template('manager_login.html')
-
-@app.route('/manager/signup', methods=['GET', 'POST'])
-def manager_signup():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-
-        if username in managers:
-            return "Username already exists. Please choose a different username."
-
-        # Store the manager details in the dictionary
-        managers[username] = {'username': username, 'password': generate_password_hash(password)}
-        return redirect(url_for('manager_dashboard'))
-
-    return render_template('manager_signup.html')
-
-@app.route('/user/dashboard')
-def user_dashboard():
-    # This is the user dashboard page
-    return "Welcome to your user dashboard!"
-
-@app.route('/manager/dashboard')
-def manager_dashboard():
-    # This is the manager dashboard page
-    return "Welcome to your manager dashboard!"
+# ... routes for user login, signup, manager login, signup, user dashboard, and manager dashboard ...
 
 if __name__ == '__main__':
     app.run(debug=True)
